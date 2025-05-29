8a18ae6784cccb883358ec767850cbecbae969c6
Aashis719 <aashishnep10@gmail.com>
2025-05-29 20:56:51 +0545

    small changes in movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 // movie card here 
+// the movie card is the card that displays the movie information on the home page
 export default function MovieCard({ movie }) {
   return (
     <Link to={`/movie/${movie.imdbID}`}>
