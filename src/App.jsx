cb60452ebd066fb7dd190d6446ea8de3d371543d
Aashis719 <aashishnep10@gmail.com>
2025-06-06 20:56:32 +0545

    comments changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MovieDetail from './pages/MovieDetail';
-// BrowserRouter is a component that uses the HTML5 history API to keep your UI in sync with the URL
-// react router dom v6 is a library for routing in react
+
 function App() {
   return (
     <BrowserRouter>
